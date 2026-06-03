499171f7cd26bfc07a8cc2ac14f88d5883c0f27c
codewithmax <coderwithmax@gmail.com>
2026-06-04 00:37:05 +0300

    done

---
+++ main.js
@@ -0,0 +1,25 @@
+
+console.log("Hello World.")
+
+let myname = "moumin academy"
+console.log(myname)
+let age = 30
+age = 30
+ console.log("da,daydu waa: " + age)
+
+
+
+// Arrays 
+let collage = ["moumin academy","web developement","JavasCript","arrays","evening","maxamed cabdiqani","jigjiga"]
+console.log(collage[2])
+collage.push("Somali Region")
+collage.pop()
+collage.pop()
+collage.shift()
+collage.shift()
+collage.unshift("HTML")
+console.log(collage)
+
+let others = [24,"Whoami",true]
+others[0] = ["esra","amina","ahmed","zakaie",'Mohamed id']
+console.log(others)
